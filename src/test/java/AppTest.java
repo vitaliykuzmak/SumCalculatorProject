3fe7217e31ac6e119e2edfc9d4e653b6d60e1fe7
vitaliykuzmak <vitalikkuzmak98@gmail.com>
2025-01-16 20:37:26 +0100

    first

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void testAdd() {
+        App app = new App();
+        assertEquals(5, app.add(2, 3));
+    }
+}
+
